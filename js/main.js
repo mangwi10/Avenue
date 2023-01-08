0e69b32c3142c10dafbf6f0ee94ef6d569d178af
mangwi10 <mankwiyana@gmail.com>
2023-01-08 18:01:11 +0800

    Event Vue Js

--- js/main.js
+++ js/main.js
@@ -4,6 +4,12 @@ var content = new Vue({
     titleWeb: "Avenue",
     compStore: "component/store.html",
     compOrder: "component/order.html",
+    latestArrivalsProduct: [
+      "Red Blazer",
+      "Black Shirt",
+      "Jacket Jeans",
+      "Cream Blazer",
+    ],
     latestArrivalsImg: [
       "images/woman1.jpg",
       "images/jacket-jeans.jpg",
@@ -35,8 +41,63 @@ var content = new Vue({
     ],
     socmedAlt: ["Twitter", "Facebook", "Instagram", "Pinterest"],
     colorRed: "red",
+    // Order.html
+    price: "500000",
+    sizeShirt: ["S", "M", "L", "XL"],
+    choosenSizeShirt: "S",
+    cart: 0,
+    stock: 10,
+    availability: true,
+    qty: 0,
+    plusMessage:
+      "Klik tombol ini atau tekan tombol panah ke atas pada kolom disamping!",
+    minusMessage:
+      "Klik tombol ini atau tekan tombol panah ke bawah pada kolom disamping!",
   },
-  methods: {},
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    changeVariant(value) {
+      if (value === this.sizeShirt[0]) {
+        this.stock = 10;
+      } else if (value === this.sizeShirt[1]) {
+        this.stock = 5;
+      } else if (value === this.sizeShirt[2]) {
+        this.stock = 0;
+      } else if (value === this.sizeShirt[3]) {
+        this.stock = 12;
+      }
+
+      this.choosenSizeShirt = value;
+
+      if (this.stock <= 0) {
+        this.availability = false;
+      } else {
+        this.availability = true;
+      }
+    },
+    qtyPlus() {
+      if (this.qty < this.stock) {
+        this.qty += 1;
+      }
+    },
+    qtyMinus() {
+      if (this.qty > 0) {
+        this.qty -= 1;
+      }
+    },
+    buyNow() {
+      window.location.href =
+        "https://wa.me/6281558903315?text=Hallo%2C%20Saya%20ingin%20memesan%20%0AProduk%3A%20" +
+        this.latestArrivalsProduct[2] +
+        "%0ASize%3A%20" +
+        this.choosenSizeShirt +
+        "%0AJumlah%3A%20" +
+        this.qty +
+        "%0A%0ATerimakasih";
+    },
+  },
   computed: {},
 });
 
